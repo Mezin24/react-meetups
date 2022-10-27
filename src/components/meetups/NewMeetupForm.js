d2c01c209f5e855a196b96d6ee6348c716314264
Pavel Mezencev <mezencev24@mail.ru>
2022-10-27 10:38:29 +0300

    add form submit handler

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,25 +1,54 @@
+import { useRef } from 'react';
 import Card from '../UI/Card';
 import classes from './NewMeetupForm.module.css';
 
 const NewMeetupForm = () => {
+  const titleRef = useRef();
+  const imageRef = useRef();
+  const addressRef = useRef();
+  const descriptionRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const title = titleRef.current.value;
+    const image = titleRef.current.value;
+    const address = titleRef.current.value;
+    const description = titleRef.current.value;
+
+    const newMeetup = {
+      title,
+      image,
+      address,
+      description,
+    };
+
+    console.log(newMeetup);
+  };
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={handleSubmit}>
         <div className={classes.control}>
           <label htmlFor='title'>Meetup Title</label>
-          <input type='text' required id='title' />
+          <input type='text' required id='title' ref={titleRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='image'>Meetup Image</label>
-          <input type='url' required id='image' />
+          <input type='url' required id='image' ref={imageRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='address'>Address</label>
-          <input type='text' required id='address' />
+          <input type='text' required id='address' ref={addressRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='description'>Description</label>
-          <textarea type='text' required id='description' rows='5' />
+          <textarea
+            type='text'
+            required
+            id='description'
+            rows='5'
+            ref={descriptionRef}
+          />
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
